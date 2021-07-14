08f9ed4dc2cf9876dc302e626cd010b4abcb9162
Kranthi Guribilli <79071810+Kranthi-Guribilli@users.noreply.github.com>
2021-07-14 10:25:19 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main(){
+	int choice;
+	while(1){
+	printf("-------------------------------------------------------------\n");
+	printf("-------------------------------------------------------------\n");
+	printf("********WELCOME TO PHONEBOOK DIRECTORY********\n");
+	printf("\t\t***MENU***\n");
+	printf("1.ADD NEW");
+	printf("\t2.TAGS");
+	printf("\t3.MODIFY");
+	printf("\t4.SEARCH\n");
+	printf("5.DELETE");
+	printf("\t6.VIEW");
+	printf("\t7.EXIT\n");
+	printf("Enter your choice:");
+	scanf("%d",&choice);
+	switch(choice){
+	}
+}
+	return 0;
+}
